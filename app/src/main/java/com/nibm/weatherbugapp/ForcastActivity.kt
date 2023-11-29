26a7ca9af060bc534d938e26bfba1bc7e4c2ed11
Bhanuka69 <103242662+Bhanuka69@users.noreply.github.com>
2023-11-29 14:37:20 +0530

    second commit

---
+++ app/src/main/java/com/nibm/weatherbugapp/ForcastActivity.kt
@@ -0,0 +1,11 @@
+package com.nibm.weatherbugapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forcast)
+    }
+}
---
+++ app/src/main/java/com/nibm/weatherbugapp/WeatherActivity.kt
@@ -0,0 +1,11 @@
+package com.nibm.weatherbugapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Weather : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_home)
+    }
+}
